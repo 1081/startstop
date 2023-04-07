c88162c2282e719e9abf47d762346e990b872e61
Jan Sattler <jan1081@pm.me>
2023-04-07 23:18:15 +0200

    consecutive example

--- example.py
+++ example.py
@@ -1,6 +1,20 @@
 from startstop import t
 
+
+def run_a():
+    for i in range(100000):
+        i**2
+
+
+def run_b():
+    for i in range(100000):
+        i**2
+
+
 t()
-for i in range(100000):
-    i**2
+run_a()
+t()
+
+t()
+run_b()
 t()
